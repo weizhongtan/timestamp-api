c8dc0f5332bdbf98898275c426d2ec659d810c3d
wztan <wztan@hotmail.co.uk>
2016-11-14 17:51:31 +0000

    Added server code for timestamp service

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require("express")
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static("public"))
+
+app.get("/:date", function(req, res) {
+  var date = req.params.date;
+  if (date.match(/^[0-9]+$/) !== null) {
+    var resDate = new Date(Number(date)*1000);
+  } else if (!Number.isNaN(Date.parse(date))) {
+    var resDate = new Date(date)
+  } else {
+    var resDate = undefined
+  }
+  
+  console.log("Valid Date?", (!Number.isNaN(Date.parse(date))));
+  
+  var response = {
+    unix: (resDate === undefined) ? null : resDate.getTime()/1000,
+    natural: (resDate === undefined) ? null : convertMonth(resDate.getMonth()) + " " + resDate.getDate() + ", " + resDate.getFullYear()
+  }
+  
+  function convertMonth(monthNum) {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return months[monthNum]
+  }
+  res.end(JSON.stringify(response))
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
-});
+})
